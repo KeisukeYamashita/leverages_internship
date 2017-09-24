6eaf44a3dc1b6441ad0d07262643e161a0721b89
Keisuke Yamshita <19yamashita15@gmail.com>
2017-09-24 09:33:54 +0900

    circleed button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#login"
-  get "/portfolio", to: "static_pages#detail"
+  get "/portfolio", to: "static_pages#portfolio"
+  get "/promotion", to:"static_pages#promotion"
+  get "/chat", to:"static_pages#chat"
   resources :rooms
   get "/signup", to:"sessions#new"
   resources :users
