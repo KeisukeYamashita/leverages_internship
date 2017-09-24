d91c1c80036d1402c45ee580e00ba0e6dffe56ec
kenichi.fukuda <kenichi.fukuda@leverages.jp>
2017-09-24 12:03:40 +0900

    syntax error fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
 	  @user = User.new(user_params)
 	  if @user.save
-		  render :template = "rooms/index"
+		  render :template => "rooms/index"
 	  else
 		  render 'new'
 	  end
