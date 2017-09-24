2e4451406c38bae3cb9f53847cfbff08dae6ca99
Keisuke Yamshita <19yamashita15@gmail.com>
2017-09-24 09:06:39 +0900

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
+  get 'session/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#login"
-  get "/detail", to: "static_pages#detail"
+  get "/portfolio", to: "static_pages#detail"
   resources :rooms
+  get "/signup", to:"sessions#new"
+  resources :users
 end
 
